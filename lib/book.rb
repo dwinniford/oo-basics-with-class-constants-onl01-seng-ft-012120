6c00d23417632b81487c929c497e81d9df70d787
Dave Winniford <dwinniford@gmail.com>
2020-01-27 15:37:26 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,9 +1,11 @@
 class Book
   attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  attr_reader :title, :all_genres
 
   def initialize(title)
     @title = title
+    @@all_genres = []
+    @@all_genres << self.genre 
   end
 
   def turn_page
